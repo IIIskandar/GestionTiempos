f3a2861718249a171c0372d89fe026d30dc263b4
Sebastian Velez Ramirez <sebasp1234@hotmail.com>
2019-01-09 15:05:52 -0500

    login

--- front/src/app/topnav/topnav.component.spec.ts
+++ front/src/app/topnav/topnav.component.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import { TopnavComponent } from './topnav.component';
 
-describe('LoginComponent', () => {
-    let component: LoginComponent;
-    let fixture: ComponentFixture<LoginComponent>;
+describe('TopnavComponent', () => {
+    let component: TopnavComponent;
+    let fixture: ComponentFixture<TopnavComponent>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [LoginComponent]
+            declarations: [TopnavComponent]
         }).compileComponents();
     }));
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(LoginComponent);
+        fixture = TestBed.createComponent(TopnavComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
